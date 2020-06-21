ef0d2f4cd37cfb98d6e0127a011ce2381f30374d
kmarks2013 <kmarks2013@gmail.com>
2020-06-21 08:24:43 -0400

    updates import in app.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack"
 

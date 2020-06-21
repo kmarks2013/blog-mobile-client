973e0118c41064ce6224b4939610830e8c0033a3
kmarks2013 <kmarks2013@gmail.com>
2020-06-21 08:19:06 -0400

    intializes Stack constant

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack"
 
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
     <View style={styles.container}>

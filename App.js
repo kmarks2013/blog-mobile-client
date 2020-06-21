49994c74674817bfc5b68f384148974c36b8a2d5
kmarks2013 <kmarks2013@gmail.com>
2020-06-21 08:19:25 -0400

    starts to create navigator"

--- App.js
+++ App.js
@@ -7,9 +7,8 @@ const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+    </NavigationContainer>
   );
 }
 

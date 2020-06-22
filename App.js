ef912e1334549904edd8c1bdab72342d168d9fd4
kmarks2013 <kmarks2013@gmail.com>
2020-06-22 16:38:31 -0400

    imports BlogProvider from blogcontext

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack"
 import HomeScreen from './src/screens/HomeScreen';
 import IndexScreen from './src/screens/IndexScreen';
+import {BlogProvider} from './src/context/BlogContext'
 
 const Stack = createStackNavigator()
 

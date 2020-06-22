44853e633f1c2d56508541a49326e37d9abf493c
kmarks2013 <kmarks2013@gmail.com>
2020-06-22 16:25:00 -0400

    comments out home screen navigation and updates to indexscreen

--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack"
 import HomeScreen from './src/screens/HomeScreen';
+import IndexScreen from './src/screens/IndexScreen';
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen component={HomeScreen} name="Home"/>
+      <Stack.Navigator initialRouteName="Index" >
+        {/* <Stack.Screen
+          component={HomeScreen}
+          name="Home"
+          options={{title: "Blogs"}} */}
+
+        {/* /> */}
+        <Stack.Screen
+          component={IndexScreen}
+          name="Index"
+          options={{title:"Blogs"}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );

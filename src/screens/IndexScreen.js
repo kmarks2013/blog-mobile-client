0fabc5e23b6693e9cafd8493dd4bbd19388cf9a2
kmarks2013 <kmarks2013@gmail.com>
2020-06-22 17:01:01 -0400

    adds value via useContext call on BlogContext

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
+    const value = useContext(BlogContext)
     return (
         <View>
             <Text>Index Screen</Text>

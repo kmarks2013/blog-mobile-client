18cd2874f7061cca153b210228e455cb0d75aa90
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 17:59:46 -0400

    adds data and addblogpost object to context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View, FlatList } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext)
+    const {data, addBlogPost} = useContext(BlogContext)
     return (
         <View>
             <Text>Index Screen</Text>
             <FlatList
-                data={blogPosts}
+                data={data}
                 keyExtractor={(blogPosts)=> blogPosts.title}
                 renderItem={({item}) => {
                     return <Text>{item.title}</Text>

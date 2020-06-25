7cacd061eff6638925b5f2863dac41c5a927505b
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 17:58:03 -0400

    removes blogpost object

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,10 +13,6 @@ export const BlogProvider = ({children}) => {
     }
 
 
-    const blogPosts = [
-        {title: 'Blog post #1'},
-        {title: 'Blog post #2'},
-    ]
 
     return (
         <BlogContext.Provider value={blogPosts}>

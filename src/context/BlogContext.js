1920c5fd7f110740e8a5d3586db2bcfccb189696
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 17:29:56 -0400

    creates blogPosts object

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,12 @@ import React from 'react'
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({children}) => {
+
+    const blogPosts = [
+        {title: 'Blog post #1'},
+        {title: 'Blog post #2'},
+    ]
+
     return (
         <BlogContext.Provider value={ {number:5}}>
             {children}

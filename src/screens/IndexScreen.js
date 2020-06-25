d85d11d78959e3d740c5977393e78c297595ad7b
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 17:31:33 -0400

    imports flatlists

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {

d0e013de69918d68ed4a6ac9d62a80543dc837f9
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 18:00:00 -0400

    adds button and adds onpress

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
@@ -7,6 +7,7 @@ const IndexScreen = () => {
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
                 data={data}
                 keyExtractor={(blogPosts)=> blogPosts.title}

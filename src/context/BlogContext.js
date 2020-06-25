d1d8aa371fd5bc532e33c8fdb79efbb5806eb541
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 18:54:40 -0400

    adds blogRedcuer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,14 +2,12 @@ import React, {useReducer} from 'react'
 
 const BlogContext = React.createContext()
 
-export const BlogProvider = ({children}) => {
-    const [blogPosts, setBlogPosts] = useState([])
-
-    const addBlogPost = () => {
-        setBlogPosts([
-            ...blogPosts,
-            {title: `Blog Post #${blogPosts.length +1}`}
-        ])
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}]
+        default:
+            return state
     }
 
 

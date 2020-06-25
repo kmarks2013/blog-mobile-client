ebed7aa4657ec99b8e68e8ad7e55fc10f764d15d
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 17:57:33 -0400

    addds use state and initalizes blogpost state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({children}) => {
+    const [blogPosts, setBlogPosts] = useState([])
+
+    const addBlogPost = () => {
+        setBlogPosts([
+            ...blogPosts,
+            {title: `Blog Post #${blogPosts.length +1}`}
+        ])
+    }
+
 
     const blogPosts = [
         {title: 'Blog post #1'},

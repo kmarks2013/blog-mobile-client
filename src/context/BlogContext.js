11203f20f7adb79b9e3d8bbecd87ea886bb897b0
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 22:06:48 -0400

    adds delete case andd id to blogposts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,15 @@ import createDataContext from './createDataContext'
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
-            return [...state, {title: `Blog Post #${state.length + 1}`}]
+            return [
+                ...state,
+                {
+                    id: Math.floor(Math.random() *99999),
+                    title: `Blog Post #${state.length + 1}`
+                }
+            ]
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload)
         default:
             return state
     }

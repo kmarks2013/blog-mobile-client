8e584406b38a71c1156fd04c83a970c3c48e411b
kmarks2013 <kmarks2013@gmail.com>
2020-06-25 22:05:35 -0400

    adds basic styling for blogPosts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -23,4 +23,20 @@ const IndexScreen = () => {
 
 export default IndexScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon:{
+        fontSize: 24
+    }
+
+})

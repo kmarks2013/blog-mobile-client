82b8ab68114d44a716192cb1064eac02706c82c6
kmarks2013 <kmarks2013@gmail.com>
2020-07-01 14:36:12 -0400

    removes and fixes console.logs

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -5,19 +5,14 @@ import {Context} from '../context/BlogContext'
 const BlogScreen = ({route}) => {
     const {state} = useContext(Context)
     const  id = route.params.id
-    console.log("hello", id)
-
 
     const blogPost = state.find((post) =>{
         if (post.id === id){
-            console.log(post.id)
             return post
         } else {
-            console.log(' wrongid')
+            console.log('wrongid')
         }
     })
-
-    console.log(blogPost)
 
     return (
         <View>

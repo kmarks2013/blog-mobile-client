278c61aea8bb7351076c9d0fdc4271549932a18b
kmarks2013 <kmarks2013@gmail.com>
2020-07-01 16:34:28 -0400

    adds lgoic to render abutton in the top right corner of the header

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import {Context} from '../context/BlogContext'
+import { EvilIcons } from '@expo/vector-icons'
 
-const BlogScreen = ({route}) => {
+const BlogScreen = ({route, navigation}) => {
     const {state} = useContext(Context)
     const  id = route.params.id
 
@@ -12,6 +13,16 @@ const BlogScreen = ({route}) => {
         } else {
             console.log('wrongid')
         }
+    })
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+        headerRight: () => (
+            <TouchableOpacity>
+                <EvilIcons name="pencil" size={35}/>
+            </TouchableOpacity>
+            )
+        })
     })
 
     return (

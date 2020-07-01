8a49e9ccf672549de6fdfeeb70be349f5080bc68
kmarks2013 <kmarks2013@gmail.com>
2020-07-01 14:48:27 -0400

    adds NewBlogPost screen with static text

---
+++ src/screens/NewBlogPost.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {Context} from '../context/BlogContext'
+
+
+const NewBlogPost = () => {
+    return (
+        <View>
+            <Text>Add a new Blog post</Text>
+        </View>
+    )
+}
+
+export default NewBlogPost
+
+const styles = StyleSheet.create({})

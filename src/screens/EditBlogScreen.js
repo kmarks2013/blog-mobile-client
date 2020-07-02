92bcd4e46f7a81c8b4f73842b3285d676ac2e6d6
kmarks2013 <kmarks2013@gmail.com>
2020-07-02 19:03:33 -0400

    updates edit screen to render BlogPostForm and removes unnecessary code

--- src/screens/EditBlogScreen.js
+++ src/screens/EditBlogScreen.js
@@ -1,6 +1,7 @@
-import React, {useState, useContext}from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, {useContext}from 'react'
+import { StyleSheet } from 'react-native'
 import {Context} from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm'
 
 const EditBlogScreen = ({route}) => {
     const {state} = useContext(Context)
@@ -13,24 +14,9 @@ const EditBlogScreen = ({route}) => {
         }
     })
 
-    const [title, setTitle] = useState(blogPost.title)
-    const [content, setContent] = useState(blogPost.content)
-
     return (
-        <View>
-            <Text>Edit Title:</Text>
-            <TextInput
-                value={title}
-                onChangeText={(newTitle)=> setTitle(newTitle)}
-            />
-            <Text>Edit Content:</Text>
-            <TextInput
-                value={content}
-                onChangeText={(newContent)=> setContent(newContent)}
-            />
-
-        </View>
-    )
+        <BlogPostForm/>
+        )
 }
 
 export default EditBlogScreen

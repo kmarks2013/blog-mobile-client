9dc672fc3249d7d18096f61e5fd3cc0cf3266b6f
kmarks2013 <kmarks2013@gmail.com>
2020-07-02 19:17:45 -0400

    removes weird import

--- src/screens/NewBlogPost.js
+++ src/screens/NewBlogPost.js
@@ -2,7 +2,6 @@ import React, { useContext} from 'react'
 import { StyleSheet } from 'react-native'
 import {Context} from '../context/BlogContext'
 import BlogPostForm from '../components/BlogPostForm'
-import { add } from 'react-native-reanimated'
 
 
 const NewBlogPost = ({navigation}) => {

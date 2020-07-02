99e4046df844cf5ea9d862bc7c28569f48710141
kmarks2013 <kmarks2013@gmail.com>
2020-07-02 18:44:35 -0400

    sets up state for edit screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,5 +1,20 @@
-import React from 'react'
+import React, {useState, useContext}from 'react'
 import { StyleSheet, Text, View, TextInput } from 'react-native'
+import {Context} from '../context/BlogContext'
+
+const EditScreen = ({route}) => {
+    const {state} = useContext(Context)
+
+    const blogPost = state.find((post) =>{
+        if (post.id === route.params.id){
+            return post
+        } else {
+            console.log('wrongid')
+        }
+    })
+
+    const [title, setTitle] = useState(blogPost.title)
+    const [content, setContent] = useState(blogPost.content)
 
 const EditScreen = () => {
     return (

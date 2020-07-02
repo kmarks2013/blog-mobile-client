27066d52882cb7f011ef2fe25839bd33269c0e1c
kmarks2013 <kmarks2013@gmail.com>
2020-07-02 18:46:48 -0400

    adds blog post form file with basic broiler plate:

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BlogPostForm = () => {
+    return ( 
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default BlogPostForm
+
+const styles = StyleSheet.create({})

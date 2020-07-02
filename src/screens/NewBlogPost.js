1cc0e5736cc8f0f65c32ea55ad3e3efd25363531
kmarks2013 <kmarks2013@gmail.com>
2020-07-02 19:16:43 -0400

    passes onsubmit prop to blogpost form calling on addblogpost with title, content  and callback to navigate  to index

--- src/screens/NewBlogPost.js
+++ src/screens/NewBlogPost.js
@@ -2,12 +2,17 @@ import React, { useContext} from 'react'
 import { StyleSheet } from 'react-native'
 import {Context} from '../context/BlogContext'
 import BlogPostForm from '../components/BlogPostForm'
+import { add } from 'react-native-reanimated'
 
 
 const NewBlogPost = ({navigation}) => {
     const {addBlogPost} = useContext(Context)
 
-    return <BlogPostForm/>
+    return <BlogPostForm
+        onSubmit={(title, content) => {
+            addBlogPost(title,content, () => navigation.navigate('Index'))
+        } }
+        />
 
 }
 
